19d23596127a61eb202f66f216db31285378a5c7
mulhak <mulhakieran@gmail.com>
2021-04-14 17:04:15 +0100

    color tests only done..reverted to initial colors

--- script.js
+++ script.js
@@ -13,11 +13,42 @@ next.addEventListener('click', ()=> {
      if(currentActive > circles.length){
          currentActive = circles.length
      } 
-     console.log(currentActive)   
+    update()
 })
 
-prev.addEventListener('click',()=> {
-    
-});
+prev.addEventListener('click', ()=> {
+    currentActive--
+    if(currentActive < 1) {
+        currentActive = 1
 
+    }
+    
+    update()
+    
+})
+
+function update() {
+    
+   circles.forEach((circle, idx) =>{
+       if(idx < currentActive) {
+           circle.classList.add('active')
+       } else {
+           circle.classList.remove('active')
+       }
+
+   })  
+   const actives = document.querySelectorAll('.active')
+   progress.style.width = (actives.length -1)
+   / (circles.length -1)*100 +'%'
+
+   if(currentActive ===1) {
+       prev.disabled = true
+   } else if(currentActive === circles.length) {
+       next.disabled = true
+   } else {
+       prev.disabled = false
+       next.disabled = false
+   }
+
+}
 
